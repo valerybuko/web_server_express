f50a17c045cad6c79e36a22082696ec190b8e0f3
valery.buka <valpal314@gmail.com>
2020-02-19 17:41:20 +0300

    add middleware catching errors in user routes

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -7,58 +7,68 @@ import {
     getUserWithID,
     updateUser
 } from "../services/user-service";
+import badRequestErrorHandler from '../errors/BadRequestErrorHandler';
 
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const {check, validationResult} = require('express-validator/check');
 
 module.exports = () => {
-    router.get('/users', async (req, res) => {
-        const allUsers = await getAllUsers().catch(err => res.status(HttpStatus.BAD_REQUEST).send());
+    router.get('/users', badRequestErrorHandler(async (req, res) => {
+            const allUsers = await getAllUsers();
 
-        if (!allUsers.length) {
-             return res.status(HttpStatus.NOT_FOUND).send();
-        }
+            if (!allUsers.length) {
+                return res.status(HttpStatus.NOT_FOUND).send();
+            }
 
-        return res.status(HttpStatus.OK).send(allUsers);
-    });
+            res.status(HttpStatus.OK).send(allUsers);
+        })
+    );
 
-    router.get('/user', async (req, res) => {
-        const user = await getUserWithID(req.query.id).catch(err => res.status(HttpStatus.BAD_REQUEST).send());
+    router.get('/user', badRequestErrorHandler(async (req, res) => {
+            const user = await getUserWithID(req.query.id);
 
-        if(!user) {
-            return res.status(HttpStatus.NOT_FOUND).send();
-        }
+            if (!user) {
+                return res.status(HttpStatus.NOT_FOUND).send();
+            }
 
-        return res.status(HttpStatus.OK).send(user);
-    });
+            res.status(HttpStatus.OK).send(user);
+        })
+    );
 
-    router.get('/email', async (req, res) => {
-        const user = await getUserByEmail(req.body.email).catch(err => res.status(HttpStatus.BAD_REQUEST).send());
+    router.get('/email', badRequestErrorHandler(async (req, res) => {
+            const user = await getUserByEmail(req.body.email);
 
-        if(!user) {
-            return res.status(HttpStatus.NOT_FOUND).send();
-        }
+            if (!user) {
+                return res.status(HttpStatus.NOT_FOUND).send();
+            }
 
-        return res.status(HttpStatus.OK).send(user);
-    });
+            res.status(HttpStatus.OK).send(user);
+        })
+    );
 
     router.put('/user', [
-        check('email').normalizeEmail().isEmail(),
-        check('password', 'Enter a password with five or more characters').isLength({min: 5})
-    ], async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(HttpStatus.BAD_REQUEST).json({errors: errors.array()});
-        } else {
-            const result = await updateUser(req.body).catch(err => console.log(err));
-            return res.status(HttpStatus.CREATED).send(result);
-        }
-    });
+            check('email').normalizeEmail().isEmail(),
+            check('password', 'Enter a password with five or more characters').isLength({min: 5})
+        ], badRequestErrorHandler(async (req, res) => {
+            const errors = validationResult(req);
 
-    router.delete('/user', async (req, res) => {
-        await deleteUser(req.query.id).catch(err => res.send(HttpStatus.BAD_REQUEST));
-        return res.status(HttpStatus.OK).send();
-    });
+            if (!errors.isEmpty()) {
+                return res.status(HttpStatus.BAD_REQUEST).json({errors: errors.array()});
+            }
+
+            const result = await updateUser(req.body);
+
+            res.status(HttpStatus.CREATED).send(result);
+        })
+    );
+
+    router.delete('/user',
+        badRequestErrorHandler(async (req, res) => {
+            await deleteUser(req.query.id);
+
+            res.status(HttpStatus.OK).send();
+        })
+    );
 
     return router;
 }
